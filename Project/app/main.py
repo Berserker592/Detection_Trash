baf8ec58c0a91d21d6a9cef4e8b478b455824b7a
Berserker592 <paulguamani5921@gmail.com>
2025-02-11 16:07:42 -0500

    Update main.py

--- Project/app/main.py
+++ Project/app/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
 
 @app.get("/")
 def get_frontend():
-    return FileResponse("/static/index.html")
+    return FileResponse("static/index.html")
 
 id_select = "None"
 #Simulacion de Dispositivos
